3e9538952f74095ac5e42692b499915bdde8585a
aseppp <saepudinasep86@gmail.com>
2023-03-12 04:44:53 +0700

    info box adjustment

--- components/Info.jsx
+++ components/Info.jsx
@@ -4,24 +4,24 @@ const Info = ({ ip, city, region, postal, timezone, isp }) => {
   return (
     <div className='grid text-center md:text-left md:grid-cols-4 place-content-center'>
       <div>
-        <p className='uppercase mb-3 text-grayy'>ip address</p>
+        <p className='uppercase mb-3 text-grayy font-medium'>ip address</p>
         <h1 className='text-darkGray text-2xl font-medium'>{ip}</h1>
       </div>
 
       <div>
-        <p className='uppercase mb-3 text-grayy'>Location</p>
+        <p className='uppercase mb-3 text-grayy font-medium'>Location</p>
         <h1 className='text-darkGray text-2xl font-medium'>
-          {city} {region} {postal}
+          {city}, {region}, {postal}
         </h1>
       </div>
 
       <div>
-        <p className='uppercase mb-3 text-grayy'>timezone</p>
-        <h1 className='text-darkGray text-2xl font-medium'>{timezone}</h1>
+        <p className='uppercase mb-3 text-grayy font-medium'>timezone</p>
+        <h1 className='text-darkGray text-2xl font-medium'>UTC {timezone}</h1>
       </div>
 
       <div>
-        <p className='uppercase mb-3 text-grayy'>isp</p>
+        <p className='uppercase mb-3 text-grayy font-medium'>isp</p>
         <h1 className='text-darkGray text-2xl font-medium'>{isp}</h1>
       </div>
     </div>
