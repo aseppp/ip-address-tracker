62b3335ea803d60331532c526e1ab102eeb9074a
aseppp <saepudinasep86@gmail.com>
2023-03-12 04:31:42 +0700

    info box

--- app/page.js
+++ app/page.js
@@ -56,7 +56,14 @@ export default function Home() {
         </div>
 
         <div className='rounded-xl shadow-xl p-5 top-0 mt-40 md:mt-60 absolute left-0 right-0 mx-3 md:m-auto md:w-info bg-gray-100'>
-          <Info />
+          <Info
+            ip={data?.ip || 'No Data'}
+            city={data?.location.city || 'No Data'}
+            region={data?.location.region}
+            postal={data?.location?.postalCode}
+            timezone={data?.location?.timezone || 'No Data'}
+            isp={data?.isp || 'No Data'}
+          />
         </div>
       </div>
 
--- components/Info.jsx
+++ components/Info.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 
-const Info = () => {
+const Info = ({ ip, city, region, postal, timezone, isp }) => {
   return (
     <div className='grid text-center md:text-left md:grid-cols-4 place-content-center'>
       <div>
         <p className='uppercase mb-3 text-grayy'>ip address</p>
-        <h1 className='text-darkGray text-2xl font-medium'>192.168.1.1</h1>
+        <h1 className='text-darkGray text-2xl font-medium'>{ip}</h1>
       </div>
 
       <div>
         <p className='uppercase mb-3 text-grayy'>Location</p>
-        <h1 className='text-darkGray text-2xl font-medium'>192.168.1.1</h1>
+        <h1 className='text-darkGray text-2xl font-medium'>
+          {city} {region} {postal}
+        </h1>
       </div>
 
       <div>
         <p className='uppercase mb-3 text-grayy'>timezone</p>
-        <h1 className='text-darkGray text-2xl font-medium'>192.168.1.1</h1>
+        <h1 className='text-darkGray text-2xl font-medium'>{timezone}</h1>
       </div>
 
       <div>
         <p className='uppercase mb-3 text-grayy'>isp</p>
-        <h1 className='text-darkGray text-2xl font-medium'>192.168.1.1</h1>
+        <h1 className='text-darkGray text-2xl font-medium'>{isp}</h1>
       </div>
     </div>
   );
